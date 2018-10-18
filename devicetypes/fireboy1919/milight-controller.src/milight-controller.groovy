8bdb587ab26bd72a37e3cafd21884d950cc89474
rhamblen <richardh@iname.com>
2018-10-18 08:27:38 +0100

    modification for night mode
    added tile and event call

--- devicetypes/fireboy1919/milight-controller.src/milight-controller.groovy
+++ devicetypes/fireboy1919/milight-controller.src/milight-controller.groovy
@@ -2,6 +2,8 @@
  *  MiLight / EasyBulb / LimitlessLED Light Controller
  *
  *  Copyright 2017  Rusty Phillips rusty dot phillips at gmail dot com
+ *  v1.1 origional by fireboy1919
+ *  v1.2 update to include night mode
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -27,6 +29,7 @@ metadata {
         command "pair"
         command "unpair"
         command "whiten"
+        command "nightmode"
 	}
     
     preferences {       
@@ -68,8 +71,13 @@ metadata {
            state "default", label:"White", defaultState: true, action: "whiten", icon: "st.switches.switch.off"
         }
 
-		main(["switch"])
-		details(["switch","levelSliderControl", "rgbSelector", "refresh", "pair", "unpair", "white"])
+	standardTile("night", "device.switch", width: 2, height: 2, canChangeIcon: true) {
+           state "default", label:"Night", defaultState: true, action: "nightmode", icon: "st.switches.switch.off"
+        }
+
+		
+	main(["switch"])
+		details(["switch","levelSliderControl", "rgbSelector", "refresh", "pair", "unpair", "white", "night"])
 	} 
 }
 
@@ -114,7 +122,13 @@ def unpair() {
 }
 
 def whiten() {
-   sendEvent(name: "whiten", value: java.util.UUID.randomUUID().toString())    
+   sendEvent(name: "whiten", value: java.util.UUID.randomUUID().toString())   
+   log.debug("Sent whiten")
+}
+
+def nightmode() {
+   sendEvent(name: "nightmode", value: java.util.UUID.randomUUID().toString())
+   log.debug("Sent nightmode")
 }
 
 def unknown() {
